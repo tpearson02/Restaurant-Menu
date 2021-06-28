dc6d21eb1c9c2f96e2b44bb84b0dc6c029e4e632
ranumj <gman.0505@gmail.com>
2021-06-28 17:00:56 -0500

    Restaurant Menu App Studio

---
+++ RestaurantMenu/Menu.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RestaurantMenu
+{
+    public class Menu
+    {
+        // Declare List to Store Menu Items
+       private List<MenuItem> MenuItems;
+        public Menu()
+        {
+            this.MenuItems = new List<MenuItem>();
+        }
+
+        // Method to Add Items to List
+       public void AddItem(double price, string description , string category)
+        {
+            MenuItem AddedItem = new MenuItem(price, description, category);
+            MenuItems.Add(AddedItem);
+        }
+
+        // Method to Print Item in List
+        public void PrintItems()
+        {
+            foreach(MenuItem menuItem in MenuItems)
+            {
+                Console.WriteLine($"{menuItem.ItemCategory} | {menuItem.ItemDescription} | {menuItem.ItemPrice} | {menuItem.itemDateTime}");
+            }
+        }
+    }
+}
---
+++ RestaurantMenu/MenuItem.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RestaurantMenu
+{
+    public class MenuItem
+
+        // Fields
+    {
+        public double ItemPrice { get; set; }
+        public string ItemDescription { get; set; }
+        public string ItemCategory { get; set; }
+
+        public DateTime itemDateTime;
+
+
+        // Constructor
+        public MenuItem(double price, string description, string category)
+        {
+            this.ItemPrice = price;
+            this.ItemDescription = description;
+            this.ItemCategory = category;
+            this.itemDateTime = DateTime.Today;
+        }
+    }
+}
